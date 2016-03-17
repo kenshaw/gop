7efc0803053dc147777ea544968082e574fe4cc0
Kenneth Shaw <kenneth.shaw@knq.io>
2016-03-17 14:20:14 +0700

    Quick add for support for dex files contained within the file

--- gop.go
+++ gop.go
@@ -14,11 +14,45 @@ import (
 	"github.com/ryanuber/go-glob"
 )
 
+const (
+	TempPrefix = "gop"
+)
+
 // ArgType are the command line arguments.
 type ArgType struct {
 	JarFile string   `arg:"positional,required,help:jar file"`
 	Extra   []string `arg:"positional,help:extra parameters to pass to javap"`
 	Glob    string   `arg:"--only,help:only process matching classes matching specified glob"`
+	Dex2Jar string   `arg:"--dex2jar,help:path to dex2jar executable"`
+}
+
+// processDex processes a dex file.
+func processDex(args *ArgType, dexPath, tmpDir, origName string) error {
+	var err error
+
+	// get temp file name
+	f, err := ioutil.TempFile(tmpDir, TempPrefix)
+	if err != nil {
+		return err
+	}
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+	err = os.Remove(f.Name())
+	if err != nil {
+		return err
+	}
+
+	// pass dex through dex2jar
+	c := exec.Command(args.Dex2Jar, "-o", f.Name(), dexPath)
+	_, err = c.Output()
+	if err != nil {
+		return err
+	}
+
+	// process jar
+	return processJar(args, f.Name(), tmpDir, origName)
 }
 
 // processJar processes a jar file.
@@ -47,7 +81,7 @@ func processJar(args *ArgType, jarPath, tmpDir, origName string) error {
 
 		// process file
 		switch ext[1:] {
-		case "jar", "aar":
+		case "jar", "apk", "aar", "dex":
 			// open file in zip for reading
 			fr, err := f.Open()
 			if err != nil {
@@ -56,7 +90,7 @@ func processJar(args *ArgType, jarPath, tmpDir, origName string) error {
 			defer fr.Close()
 
 			// open destination file
-			tf, err := ioutil.TempFile(tmpDir, "gop")
+			tf, err := ioutil.TempFile(tmpDir, TempPrefix)
 			if err != nil {
 				return err
 			}
@@ -68,8 +102,15 @@ func processJar(args *ArgType, jarPath, tmpDir, origName string) error {
 				return err
 			}
 
-			// defer processing of jar
-			defer processJar(args, tf.Name(), tmpDir, path.Join(origName, f.Name))
+			// if it's a dex, convert it
+			if ext[1:] == "dex" {
+				if args.Dex2Jar != "" {
+					defer processDex(args, tf.Name(), tmpDir, path.Join(origName, f.Name))
+				}
+			} else {
+				// process jar
+				defer processJar(args, tf.Name(), tmpDir, path.Join(origName, f.Name))
+			}
 
 		case "class":
 			// determine class name
@@ -104,12 +145,37 @@ func processJar(args *ArgType, jarPath, tmpDir, origName string) error {
 	return nil
 }
 
+// findDex2Jar attempts to find dex2jar.
+func findDex2Jar(args *ArgType) {
+	dxHome := os.Getenv("DEXTOOLS_HOME")
+	if dxHome == "" {
+		return
+	}
+
+	// look for the dex2jar / dex2jar.sh file
+	for _, n := range []string{"d2j-dex2jar.sh", "dex2jar.sh", "dex2jar"} {
+		p := path.Join(dxHome, n)
+		f, err := os.Stat(p)
+		if err != nil {
+			continue
+		} else if f.IsDir() {
+			continue
+		}
+		args.Dex2Jar = p
+	}
+}
+
 func main() {
 	var err error
 
 	// parse args
 	args := &ArgType{}
 	arg.MustParse(args)
+
+	// attempt to find dextools if path not specified
+	if args.Dex2Jar == "" {
+		findDex2Jar(args)
+	}
 
 	// replace ^ with - (issue with go-arg)
 	for i, v := range args.Extra {
@@ -122,7 +188,7 @@ func main() {
 	}
 
 	// create tempDir
-	tmpDir, err := ioutil.TempDir(os.TempDir(), "gop")
+	tmpDir, err := ioutil.TempDir(os.TempDir(), TempPrefix)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
